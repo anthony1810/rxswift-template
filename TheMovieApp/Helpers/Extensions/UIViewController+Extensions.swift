fc663634ac702a2fcdc3040d265686844d168073
Anthony Tran <qquang269@gmail.com>
2022-09-09 15:58:11 +0700

    add R.Swift run script

--- TheMovieApp/Helpers/Extensions/UIViewController+Extensions.swift
+++ TheMovieApp/Helpers/Extensions/UIViewController+Extensions.swift
@@ -31,7 +31,6 @@ extension UIViewController {
     func showAlert(title: String? = nil, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: R.string.localizable.alertConfirm(), style: .default, handler: nil))
-        
         self.present(alert, animated: true)
     }
 }
